a6dbea88d5591b88bb3e021e1912d6435bdf5f1d
namfonnnnn <sp.sudaratphiwon@hotmail.com>
2019-01-20 13:25:18 +0700

    แก้ไขฟังก์ชั่น

--- app/views/manage/activity_add.blade.php
+++ app/views/manage/activity_add.blade.php
@@ -13,39 +13,6 @@
 @stop
 @section('content')
 
-<?php 
-
-
-   // if(isset($activity))
-   // {
-   //    $text_activityname = Input::old('activityname') ?? $activity->activity_name ?? '';
-   //    $text_activitydetail = Input::old('activitydetail') ?? $activity->description ?? '';
-   //    $text_daystart = Input::old('daystart') ?? $activity->day_startNomalFormat() ?? '';
-   //    $text_dayend = Input::old('dayend') ?? $activity->day_startNomalFormat() ?? '';
-   //    $text_timestart = Input::old('timestart') ?? $activity->time_start ?? '';
-   //    $text_timeend = Input::old('timeend') ?? $activity->time_end ?? '';
-   //    $checkbox_term = Input::old('term') ?? $activity->term_year ?? '';
-   //    $text_sector = Input::old('sector') ?? $activity->sector ?? '';
-   //    $check_teacher =  Input::old('teacher') ?? $activity->teacherJson() ?? array();
-   //    $text_location = Input::old('location') ?? $activity->location ?? '';
-   //    $check_years =  Input::old('years') ?? $activity->studentJson() ?? array();
-   // }else{
-   //    $text_activityname = Input::old('activityname') ?? '';
-   //    $text_activitydetail = Input::old('activitydetail') ?? '';
-   //    $text_daystart = Input::old('daystart') ?? '';
-   //    $text_dayend = Input::old('dayend') ?? '';
-   //    $text_timestart = Input::old('timestart') ?? '';
-   //    $text_timeend = Input::old('timeend') ?? '';
-   //    $checkbox_term = Input::old('term') ?? '';
-   //    $text_sector = Input::old('sector') ?? '';
-   //    $check_teacher =  Input::old('teacher') ?? array();
-   //    $text_location = Input::old('location') ?? '';
-   //    $check_years =  Input::old('years') ?? array();
-   // }
-   
-
-?>
-
 <form class="form-horizontal" autocomplete="off" enctype="multipart/form-data" method="post">
    <div class="container">
       @if(isset($activity))
@@ -61,7 +28,7 @@
             <div class="form-group">
                <label for="name">ชื่อกิจกรรม</label>
                <input type="text" class="form-control {{$errors->has('activityname') ? 'is-invalid' : ''}} " id="activityname" name="activityname" value="{{$text_activityname}}" placeholder ="ชื่อกิจกรรม" >
-               <small id="emailHelp" class="form-text text-danger">{{$errors->first('activityname')}}</small>
+               <small class="form-text text-danger">{{$errors->first('activityname')}}</small>
             </div>
             <div class="form-group">
                <label for="exampleFormControlTextarea1">รายละเอียดเพิ่มเติม (ระบุหรือไม่ระบุก็ได้)</label>
@@ -72,14 +39,14 @@
                   <div class="form-group">
                      <label for="name">วันที่เริ่มกิจกรรม</label>
                      <input type="text" class="form-control datetimepicker-input {{$errors->has('daystart') ? 'is-invalid' : ''}}" id="daystart" name="daystart" value="{{$text_daystart}}" data-toggle="datetimepicker" data-target="#daystart" placeholder ="00/00/0000">
-                     <small id="emailHelp" class="form-text text-danger">{{$errors->first('daystart')}}</small>
+                     <small class="form-text text-danger">{{$errors->first('daystart')}}</small>
                   </div>
                </div>
                <div class="col">
                   <div class="form-group">
                      <label for="name">วันที่สิ้นสุดกิจกรรม</label>
                      <input type="text" class="form-control datetimepicker-input {{$errors->has('dayend') ? 'is-invalid' : ''}}" id="dayend" name="dayend"  value="{{$text_dayend}}" data-toggle="datetimepicker" data-target="#dayend" placeholder ="00/00/0000">
-                     <small id="emailHelp" class="form-text text-danger">{{$errors->first('dayend')}}</small>
+                     <small class="form-text text-danger">{{$errors->first('dayend')}}</small>
                   </div>
                </div>
             </div>
@@ -88,14 +55,14 @@
                   <div class="form-group">
                      <label for="name">เวลาที่เริ่มกิจกรรม</label>
                      <input type="text" class="form-control datetimepicker-input {{$errors->has('timestart') ? 'is-invalid' : ''}}" id="timestart" name="timestart" value="{{$text_timestart}}" data-toggle="datetimepicker"  data-target="#timestart" placeholder ="00:00">
-                     <small id="emailHelp" class="form-text text-danger">{{$errors->first('timestart')}}</small>
+                     <small class="form-text text-danger">{{$errors->first('timestart')}}</small>
                   </div>
                </div>
                <div class="col">
                   <div class="form-group">
                      <label for="name">เวลาที่สิ้นสุดกิจกรรม</label>
                      <input type="text" class="form-control datetimepicker-input {{$errors->has('timeend') ? 'is-invalid' : ''}}" id="timeend" name="timeend" value="{{$text_timeend}}" data-toggle="datetimepicker" data-target="#timeend"  placeholder ="00:00">
-                     <small id="emailHelp" class="form-text text-danger">{{$errors->first('timeend')}}</small>
+                     <small class="form-text text-danger">{{$errors->first('timeend')}}</small>
                   </div>
                </div>
             </div>
@@ -109,14 +76,14 @@
                         <option value="2" {{($checkbox_term==2)?'selected':''}} >ภาคการศึกษาที่ 2</option>
                         <option value="3" {{($checkbox_term==3)?'selected':''}} >ภาคการศึกษาที่ 3</option>
                      </select>
-                     <small id="emailHelp" class="form-text text-danger">{{$errors->first('term')}}</small>
+                     <small class="form-text text-danger">{{$errors->first('term')}}</small>
                   </div>
                </div>
                <div class="col">
                   <div class="form-group">
                      <label for="name">ปีการศึกษา</label>
                      <input type="number" class="form-control {{$errors->has('sector') ? 'is-invalid' : ''}}" id="sector" name="sector" value="{{$text_sector}}"  placeholder ="ปีการศึกษา">
-                     <small id="emailHelp" class="form-text text-danger">{{$errors->first('sector')}}</small>
+                     <small class="form-text text-danger">{{$errors->first('sector')}}</small>
                   </div>
                </div>
             </div>
@@ -207,13 +174,13 @@
                   </div>
                </div>
             </div>
-            <small id="emailHelp" class="form-text text-danger">{{$errors->first('teacher')}}</small>
+            <small class="form-text text-danger">{{$errors->first('teacher')}}</small>
 
             <br>
             <div class="form-group">
                <label for="name">สถานที่จัดกิจกรรม</label>
                <input type="text" class="form-control {{$errors->has('location') ? 'is-invalid' : ''}}" id="location" name="location" value="{{$text_location}}" placeholder ="สถานที่จัดกิจกรรม" >
-               <small id="emailHelp" class="form-text text-danger">{{$errors->first('location')}}</small>
+               <small class="form-text text-danger">{{$errors->first('location')}}</small>
                <!-- <select id="location_id" name="location_id" class="form-control">
                   <option value="2">อาคารเรียนรวม7  ( บริเวณด้านหลังอาคารเรียนรวม7 )</option>
                   <option value="3">โรงอาหาร4  ( บริเวณด้านในโรงอาหาร4 )</option>
@@ -265,7 +232,7 @@
                   </div>
                </div>
             </div>
-            <small id="emailHelp" class="form-text text-danger">{{$errors->first('years')}}</small>
+            <small class="form-text text-danger">{{$errors->first('years')}}</small>
 
             <br>
             <button type="submit" class="btn btn-success">บันทึก</button>
