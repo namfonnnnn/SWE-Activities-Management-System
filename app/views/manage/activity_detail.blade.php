33a4e93a099718e715c5148af1a3aadc8682723d
DESKTOP-5D1H62T\User <chid2539@icloud.com>
2019-01-25 23:49:54 +0700

    create frontend

--- app/views/manage/activity_detail.blade.php
+++ app/views/manage/activity_detail.blade.php
@@ -4,6 +4,16 @@
 @stop
 
 @section('content')
-    <p>-รายละเอียดกิจกรรม</p>
+<div class="container">
+    <div class="card mb-3"  style="margin-top:50px; width: 60rem; height:20rem; ">
+        <img class="card-img-top" src="{{asset('assets/image/logo.png')}}" width="250" height="330"  alt="Card image cap">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+        </div>
+    </div>
+                
+</div>
 
 @stop
