0bdc174796006715cfaea3d281b8453629a99d5a
namfonnnnn <sp.sudaratphiwon@hotmail.com>
2019-01-26 01:59:49 +0700

    แก้ไข pagination

--- app/views/partials/pagination.blade.php
+++ app/views/partials/pagination.blade.php
@@ -2,11 +2,11 @@
 @if ($paginator->getLastPage() > 1)
     <nav aria-label="Page navigation example">
         <ul class="pagination">
-            <li class="page-item {{($paginator->getCurrentPage() == 1) ? ' disabled' : '' }}"><a href="{{ $paginator->getUrl(1) }}" class="page-link">Previous</a></li>
+            <li class="page-item {{($paginator->getCurrentPage() == 1) ? ' disabled' : '' }}"><a href="{{ $paginator->getUrl(1) }}" class="page-link">ก่อนหน้า</a></li>
             @for ($i = 1; $i <= $paginator->getLastPage(); $i++)
                 <li class="page-item {{($paginator->getCurrentPage() == $i) ? ' active' : '' }}"><a href="{{ $paginator->getUrl($i) }}" class="page-link" href="#">{{ $i }}</a></li>
             @endfor
-            <li class="page-item {{($paginator->getCurrentPage() == $paginator->getLastPage()) ? ' disabled' : '' }}"><a class="page-link" href="{{ $paginator->getUrl($paginator->getCurrentPage()+1) }}">Next</a></li>
+            <li class="page-item {{($paginator->getCurrentPage() == $paginator->getLastPage()) ? ' disabled' : '' }}"><a class="page-link" href="{{ $paginator->getUrl($paginator->getCurrentPage()+1) }}">ถัดไป</a></li>
         </ul>
     </nav>
 
