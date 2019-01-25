9018bc2c2979d815e206cfb00f2c2e2f0b3d81eb
namfonnnnn <sp.sudaratphiwon@hotmail.com>
2019-01-26 01:40:37 +0700

    เพิ่มฟอร์น

--- app/views/manage/layout.blade.php
+++ app/views/manage/layout.blade.php
@@ -16,6 +16,12 @@
     <script src="{{asset('assets/bootstrap/js/bootstrap.min.js')}}"></script>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU"
       crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Sarabun" rel="stylesheet">
+    <style>
+      body{
+        font-family: 'Sarabun', sans-serif;
+      }
+    </style>
     @yield('cdn')
   </head>
 
