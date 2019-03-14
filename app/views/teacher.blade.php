bf7816d95983d88a336a5467a6044b97ca317d52
jompon123 <Forex@172.16.60.205>
2019-03-14 22:28:37 +0700

    ข้อมูลอาจารย์

--- app/views/teacher.blade.php
+++ app/views/teacher.blade.php
@@ -1,6 +1,6 @@
 @extends('manage.layout')
 @section('title')
-    อาจารย์
+    
 @stop
 
    @section('content')
@@ -9,34 +9,57 @@
       ข้อมูลอาจารย์
    </h2>
  <hr>
-
-  
+@foreach ($teachers as $teacher)
+<div class="row">
+  <div class="col-lg-4 col-md-6 col-sm-12 mb-4">
+    <div class="card card-small card-post card-post--1">
       
-   <table class="table table-striped" style="margin-top:20px">
-      <thead>
-         <tr class="table-success">
-            <th class="text-center">รูปภาพ</th>
-            <th class="text-center">ชื่อ-สกุล</th>
-            <th class="text-center">อีเมล</th>
-            <th class="text-center">เบอร์โทรศัพท์</th>
-            <th class="text-center">ห้องทำงาน</th>
-          
-         </tr>
-      </thead>
-      <tbody>
-         @foreach ($teachers as $teacher)
-            <tr>
-               <td class="text-center">   
-                  <input type="image" onerror="this.src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDcMQ6ob11JlE6Q83Akzz4X-8QYnuwuyZnkeA8xdhgH1jM3QJ9'" src="{{$teacher->getAvatar()}}" alt="x3" width="130" height="130" >  
-               </td>
-               <td class="text-center">{{ $teacher->getFullName() }}</td>
-               <td class="text-center">{{ $teacher->email }}</td>
-               <td class="text-center">{{ $teacher->tel }}</td>
-               <td class="text-center">{{ $teacher->room }}</td>
-               
-            </tr>
-         @endforeach
-      </tbody>
-   </table>
-  
+        <a href="#" class="card-post__category badge badge-pill badge-dark"></a>
+        <div class="text-center">
+          <input type="image" onerror="this.src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDcMQ6ob11JlE6Q83Akzz4X-8QYnuwuyZnkeA8xdhgH1jM3QJ9'" src="{{$teacher->getAvatar()}}" alt="x3" width="130" height="130" >  
+        </div>
+        <h6 class="text-center">ชื่อ-สกุล {{ $teacher->getFullName() }}</h6>
+        <h6 class="text-center">อีเมล {{ $teacher->email }}</h6>
+        <h6 class="text-center">เบอร์โทรศัพท์ {{ $teacher->tel }}</h6>
+        <h6 class="text-center">ห้องทำงาน {{ $teacher->room }}</h6>
+      </div>
+    </div>
+
+    <div class="col-lg-4 col-md-6 col-sm-12 mb-4">
+      <div class="card card-small card-post card-post--1">
+    
+          <a href="#" class="card-post__category badge badge-pill badge-dark"></a>
+          <div class="text-center">
+            <input type="image" onerror="this.src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDcMQ6ob11JlE6Q83Akzz4X-8QYnuwuyZnkeA8xdhgH1jM3QJ9'" src="{{$teacher->getAvatar()}}" alt="x3" width="130" height="130" >  
+          </div>
+          <h6 class="text-center">ชื่อ-สกุล {{ $teacher->getFullName() }}</h6>
+          <h6 class="text-center">อีเมล {{ $teacher->email }}</h6>
+          <h6 class="text-center">เบอร์โทรศัพท์ {{ $teacher->tel }}</h6>
+          <h6 class="text-center">ห้องทำงาน {{ $teacher->room }}</h6>
+        </div>
+      </div>
+      
+      <div class="col-lg-4 col-md-6 col-sm-12 mb-4">
+        <div class="card card-small card-post card-post--1">
+
+            <div class="text-center">
+              <input type="image" onerror="this.src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDcMQ6ob11JlE6Q83Akzz4X-8QYnuwuyZnkeA8xdhgH1jM3QJ9'" src="{{$teacher->getAvatar()}}" alt="x3" width="130" height="130" >  
+            </div>
+            <h6 class="text-center">ชื่อ-สกุล {{ $teacher->getFullName() }}</h6>
+            <h6 class="text-center">อีเมล {{ $teacher->email }}</h6>
+            <h6 class="text-center">เบอร์โทรศัพท์ {{ $teacher->tel }}</h6>
+            <h6 class="text-center">ห้องทำงาน {{ $teacher->room }}</h6>
+        </div>
+      </div>
+</div>
+@endforeach   
+
+
+      
+
+    
+      
+
+
+
 @stop
