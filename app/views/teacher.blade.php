2eeb9df18bd6092f71d7e38fb4cbcc4519519309
jompon123 <Saharat Rakdam>
2019-03-14 22:36:24 +0700

    updated list teachers

--- app/views/teacher.blade.php
+++ app/views/teacher.blade.php
@@ -1,42 +1,34 @@
 @extends('manage.layout')
 @section('title')
-    อาจารย์
+ข้อมูลอาจารย์
 @stop
 
    @section('content')
    @include('error')
-   <h2>
-      ข้อมูลอาจารย์
-   </h2>
+<style media="screen">
+
+</style>
  <hr>
+<div class="row">
+
+    @foreach ($teachers as $teacher)
+    <div class="col-md-4">
+        <div class="card" style="margin-bottom:15px;">
+          <div class="card-body">
+            <h5 class="card-title text-center"><input type="image" onerror="this.src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDcMQ6ob11JlE6Q83Akzz4X-8QYnuwuyZnkeA8xdhgH1jM3QJ9'" src="{{$teacher->getAvatar()}}" alt="x3" width="130" height="130" ></h5>
+            <p class="card-text text-center">
+                    ชื่อ - สกุล : {{ $teacher->getFullName() }} <br>
+                    อีเมล : {{ $teacher->email }} <br>
+                    เบอร์โทรศัพท์ : {{ $teacher->tel }} <br>
+                    ห้องทำงาน : {{ $teacher->room }}
+            </p>
+          </div>
+        </div>
+    </div><!-- .col-md-4 -->
+        @endforeach <!--end loog $teachger-->
+</div> <!-- .row -->
+
 
   
-      
-   <table class="table table-striped" style="margin-top:20px">
-      <thead>
-         <tr class="table-success">
-            <th class="text-center">รูปภาพ</th>
-            <th class="text-center">ชื่อ-สกุล</th>
-            <th class="text-center">อีเมล</th>
-            <th class="text-center">เบอร์โทรศัพท์</th>
-            <th class="text-center">ห้องทำงาน</th>
-          
-         </tr>
-      </thead>
-      <tbody>
-         @foreach ($teachers as $teacher)
-            <tr>
-               <td class="text-center">   
-                  <input type="image" onerror="this.src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDcMQ6ob11JlE6Q83Akzz4X-8QYnuwuyZnkeA8xdhgH1jM3QJ9'" src="{{$teacher->getAvatar()}}" alt="x3" width="130" height="130" >  
-               </td>
-               <td class="text-center">{{ $teacher->getFullName() }}</td>
-               <td class="text-center">{{ $teacher->email }}</td>
-               <td class="text-center">{{ $teacher->tel }}</td>
-               <td class="text-center">{{ $teacher->room }}</td>
-               
-            </tr>
-         @endforeach
-      </tbody>
-   </table>
-  
+
 @stop
